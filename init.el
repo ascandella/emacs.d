187def6ea6f5c06397bd31952f00baa560988cd0
Aiden Scandella <sc@ndella.com>
2015-12-10 10:22:03 -0800

    Optimize load time

--- init.el
+++ init.el
@@ -1,23 +1,30 @@
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("melpa" . "http://melpa.org/packages/")
-   t)
-  (package-initialize))
+;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+(setq gc-cons-threshold 100000000)
 
-(add-to-list 'load-path' "~/.emacs.d/config")
-(require 'ai-core)
-(require 'ai-keys)
-(require 'smart-beginning-of-line)
-(require 'ai-mouse)
-(require 'ui)
-(require 'completion)
-(require 'ai-projectile)
-(require 'pbcopy)
-(require 'ai-sublimity)
+(let ((file-name-handler-alist nil))
 
-(require 'languages)
+    (when (>= emacs-major-version 24)
+    (require 'package)
+    (add-to-list
+    'package-archives
+    '("melpa" . "http://melpa.org/packages/")
+    t)
+    (package-initialize))
+
+    (add-to-list 'load-path' "~/.emacs.d/config")
+    (require 'ai-core)
+    (require 'ai-keys)
+    (require 'smart-beginning-of-line)
+    (require 'ai-mouse)
+    (require 'ui)
+    (require 'completion)
+    (require 'ai-projectile)
+    (require 'pbcopy)
+    (require 'ai-sublimity)
+
+    (require 'languages)
+)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
